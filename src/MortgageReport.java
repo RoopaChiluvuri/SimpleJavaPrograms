6e1dc986f3b8c44b2db8564956d4a04c035905a8
RoopaChiluvuri <roopasree2509@gmail.com>
2024-07-24 13:38:12 -0700

    all refactorings done

--- src/MortgageReport.java
+++ src/MortgageReport.java
@@ -2,23 +2,26 @@ import java.text.NumberFormat;
 
 public class MortgageReport {
 
+    private final NumberFormat currency;
     private MortgageCalculator calculator;
 
     public MortgageReport(MortgageCalculator calculator) {
         this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance();
     }
 
     public void printPaymentSchedule() {
         System.out.println();
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("-----------------");
-        for (double balance: calculator.getRemainingBalances())
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        for (double balance: calculator.getRemainingBalances()) {
+            System.out.println(currency.format(balance));
+        }
     }
 
     public void printMortgage() {
         double mortgage = calculator.calculateMortgage();
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        String mortgageFormatted = currency.format(mortgage);
         System.out.println("\nMORTGAGE");
         System.out.println("----------");
         System.out.println("Monthly Payments: " + mortgageFormatted);
