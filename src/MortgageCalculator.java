fbf82b944e9a0e04a113c6666bb0e9609e2d4a27
RoopaChiluvuri <roopasree2509@gmail.com>
2024-07-24 13:10:03 -0700

    extracted duplicate logic by creating getters

--- src/MortgageCalculator.java
+++ src/MortgageCalculator.java
@@ -3,7 +3,7 @@ import static java.lang.Math.pow;
 public class MortgageCalculator {
     public final static byte MONTHS_IN_YEAR = 12;
     public final static byte PERCENT = 100;
-
+  
     private int principal;
     private float annualInterest;
     private byte years;
@@ -16,8 +16,8 @@ public class MortgageCalculator {
 
     public double calculateMortgage() {
 
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-        short numberOfPayments = (short)(years * MONTHS_IN_YEAR);
+        float monthlyInterest = getMonthlyInterest();
+        short numberOfPayments = (short) getNoOfPayments();
 
         double mortgage = (principal * ((monthlyInterest * pow(1+monthlyInterest,numberOfPayments))
                 / (pow(1+monthlyInterest, numberOfPayments)-1)) );
@@ -27,14 +27,21 @@ public class MortgageCalculator {
 
     public double calculateBalance(short numberOfPaymentsMade){
 
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-        short numberOfPayments = (short)(years * MONTHS_IN_YEAR);
+        float monthlyInterest = getMonthlyInterest();
+        short numberOfPayments = (short) getNoOfPayments();
 
         double balance = principal
                 * (pow(1 + monthlyInterest, numberOfPayments) - pow(1 + monthlyInterest, numberOfPaymentsMade))
                 / (pow(1 + monthlyInterest, numberOfPayments) - 1);
 
         return balance;
+    }
+    private float getMonthlyInterest() {
+        return annualInterest / PERCENT / MONTHS_IN_YEAR;
+    }
+
+    private int getNoOfPayments() {
+        return years * MONTHS_IN_YEAR;
     }
 
     public short getYears() {
